897a18c443b6e899ece074e221b643a94ca84d73
chrislim1234 <190047@hkis.edu.hk>
2019-05-24 10:31:05 +0800

    Add files via upload

---
+++ Suit.java
@@ -0,0 +1,17 @@
+public enum Suit {
+
+HEARTS(" Hearts"),
+SPADES(" Spades"),
+DIAMONDS(" Diamonds"),
+CLUBS(" Clubs");
+
+private final String suitText;
+
+private Suit(String suitText){
+    this.suitText = suitText;
+}
+
+public String PrintSuitText(){
+    return suitText;
+}
+}
